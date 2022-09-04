47c360ae56bd31bf4b8f20a7cab61a97af85009d
amanraj-29 <amanraj.a@prodapt.com>
2022-09-04 11:41:05 +0530

    appjs

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ __dirname = '.';
 // app.use(cookieParser());
 // app.use(compression({ threshold: 0 }));
 
-app.use(express.static(path.join(__dirname, './dist/unlock-app')));
+app.use(express.static(path.join(__dirname, './dist/aws-s3-bucket')));
 
 app.get('*', (req, res) => {
     res.header('Referrer-Policy', 'origin');
-    res.status(200).sendFile(path.resolve(__dirname, './dist/unlock-app/index.html'));
+    res.status(200).sendFile(path.resolve(__dirname, './dist/aws-s3-bucket/index.html'));
 });
 
 const port = (3000);
